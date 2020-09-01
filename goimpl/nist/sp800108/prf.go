725087e337799687c2df1e528d32118299c83f43
Luke Kennedy <luke.l.kennedy@gmail.com>
2020-09-02 00:01:12 +1000

    Implement SCP03-specific version of KDF

--- goimpl/nist/sp800108/prf.go
+++ goimpl/nist/sp800108/prf.go
@@ -1,9 +1,12 @@
 package sp800108
 
 import (
+	"crypto/aes"
 	"crypto/hmac"
 	"fmt"
 	"hash"
+
+	"github.com/aead/cmac" // FIXME: look into this implementation security-wise
 )
 
 // PRF is a Pseudorandom Function acceptable to the NIST KBKDF
@@ -54,3 +57,31 @@ func (p *PRFHMAC) Compute(key, data []byte) ([]byte, error) {
 }
 
 func (p *PRFHMAC) unimplementableNISTSP800108PRF() {}
+
+// PRFCMAC is the CMAC implementation of PRF
+type PRFCMAC struct{}
+
+// OutputSizeBytes indicates the output size of the PRF in bytes, non-byte multiples of output length are not supported
+func (p *PRFCMAC) OutputSizeBytes() uint {
+	return 128
+}
+
+// Compute generatesd new data from a key and keying material data
+func (p *PRFCMAC) Compute(key, data []byte) ([]byte, error) {
+	aesCipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	h, err := cmac.New(aesCipher)
+	if err != nil {
+		return nil, err
+	}
+	_, err = h.Write(data)
+	if err != nil {
+		return nil, err
+	}
+	out := h.Sum(nil)
+	return out, nil
+}
+
+func (p *PRFCMAC) unimplementableNISTSP800108PRF() {}
--- goimpl/scp03/kdf.go
+++ goimpl/scp03/kdf.go
@@ -27,8 +27,22 @@ const (
 	DDCSRMAC DataDerivationConstant = 0b00000111
 )
 
+// KDFOutputLength is a valid output length for the KDF
+type KDFOutputLength uint16
+
+const (
+	// KDFOutput64 is a 64 bit output from the KDF
+	KDFOutput64 = 0x0040
+	// KDFOutput128 is a 128 bit output from the KDF
+	KDFOutput128 = 0x0080
+	// KDFOutput192 is a 192 bit output from the KDF
+	KDFOutput192 = 0x00C0
+	// KDFOutput256 is a 256 bit output from the KDF
+	KDFOutput256 = 0x0100
+)
+
 // Derive derives data from a base key and input data
-func (k *KDF) Derive(key []byte, rawKDF sp800108.KDF, label [11]byte, ddc DataDerivationConstant) ([]byte, error) {
+func (k *KDF) Derive(key []byte, rawKDF sp800108.KDF, label [11]byte, ddc DataDerivationConstant, length KDFOutputLength, context []byte /*FIXME: "further specified"*/) ([]byte, error) {
 	if key == nil || rawKDF == nil {
 		return nil, fmt.Errorf("KDF: nil parameters")
 	}
@@ -38,6 +52,20 @@ func (k *KDF) Derive(key []byte, rawKDF sp800108.KDF, label [11]byte, ddc DataDe
 	default:
 		return nil, fmt.Errorf("KDF: Invalid data derivation constant: %x", ddc)
 	}
-
-	return nil, fmt.Errorf("not implemented")
+	lengthData := []byte{0, 0}
+	switch length {
+	case KDFOutput64:
+		lengthData[1] = 0x40
+	case KDFOutput128:
+		lengthData[1] = 0x80
+	case KDFOutput192:
+		lengthData[1] = 0xC0
+	case KDFOutput256:
+		lengthData[0] = 0x01
+	default:
+		return nil, fmt.Errorf("KDF: Invalid output length: %d", length)
+	}
+	prf := &sp800108.PRFCMAC{}
+	ordering := []sp800108.InputStringOrdering{sp800108.InputOrderLabel, sp800108.InputOrderEmptySeparator, sp800108.InputOrderL, sp800108.InputOrderCounter, sp800108.InputOrderCounter}
+	return rawKDF.Derive(prf, sp800108.CounterLength8, key, label[:], context, lengthData, ordering)
 }
