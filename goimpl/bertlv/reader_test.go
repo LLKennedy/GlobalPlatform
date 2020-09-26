603031b2bbe6fa98282703c966a02222d14b61a5
Luke Kennedy <luke.l.kennedy@gmail.com>
2020-09-26 18:44:22 +1000

    Test reader and writer functions

--- goimpl/bertlv/reader_test.go
+++ goimpl/bertlv/reader_test.go
@@ -1,6 +1,7 @@
 package bertlv
 
 import (
+	"bytes"
 	"io"
 	"testing"
 
@@ -39,7 +40,22 @@ func TestReader_Read(t *testing.T) {
 		wantObject    Object
 		assertion     assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "example bitstring",
+			fields: fields{
+				data: bytes.NewReader([]byte{0x03, 0x07, 0x04, 0x0A, 0x3B, 0x5F, 0x29, 0x1C, 0xD0}),
+			},
+			assertion:     assert.NoError,
+			wantBytesRead: 9,
+			wantObject: Object{
+				Tag: Tag{
+					Class:  TagClassUniversal,
+					Number: 3, // TODO: constants for known tags like BOOLEAN, INTEGER, BITSTRING?
+				},
+				Length: 7,
+				Value:  []byte{0x04, 0x0A, 0x3B, 0x5F, 0x29, 0x1C, 0xD0},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
--- goimpl/bertlv/writer_test.go
+++ goimpl/bertlv/writer_test.go
@@ -37,21 +37,39 @@ func TestWriter_Write(t *testing.T) {
 	}
 	tests := []struct {
 		name             string
-		fields           fields
 		args             args
 		wantBytesWritten int
 		assertion        assert.ErrorAssertionFunc
+		wantOutput       []byte
 	}{
-		// TODO: Add test cases.
+		{
+			name:      "example bitstring",
+			assertion: assert.NoError,
+			args: args{
+				obj: Object{
+					Tag: Tag{
+						Class:               TagClassUniversal,
+						ConstructedEncoding: false,
+						Number:              3,
+					},
+					Length: 7,
+					Value:  []byte{0x04, 0x0A, 0x3B, 0x5F, 0x29, 0x1C, 0xD0},
+				},
+			},
+			wantBytesWritten: 9,
+			wantOutput:       []byte{0x03, 0x07, 0x04, 0x0A, 0x3B, 0x5F, 0x29, 0x1C, 0xD0},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
 			w := Writer{
-				output: tt.fields.output,
+				output: buf,
 			}
 			gotBytesWritten, err := w.Write(tt.args.obj)
 			tt.assertion(t, err)
 			assert.Equal(t, tt.wantBytesWritten, gotBytesWritten)
+			assert.Equal(t, tt.wantOutput, buf.Bytes())
 		})
 	}
 }
